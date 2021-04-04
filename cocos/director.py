31ed376cf3e7e0816b9a6490f7662d1830f56072
harjay12 <57065385+harjay12@users.noreply.github.com>
2021-04-04 13:29:32 -0400

    reset

--- cocos/director.py
+++ cocos/director.py
@@ -596,6 +596,10 @@ class Director(event.EventDispatcher):
         """
         # physical view size
         pw, ph = width, height
+        viewportw, viewporth = self.window.get_viewport_size()
+        if viewportw > pw:
+            pw = viewportw
+            ph = viewporth
         # virtual (desired) view size
         vw, vh = self.get_window_size()
         # desired aspect ratio
