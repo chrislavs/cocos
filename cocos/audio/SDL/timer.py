06beee34f132325e53e10120cc85a864b31e55a7
Trevor <truffini@student.bridgew.edu>
2021-04-07 10:51:12 -0400

    practice

--- cocos/audio/SDL/timer.py
+++ cocos/audio/SDL/timer.py
@@ -6,17 +6,15 @@
 __docformat__ = 'restructuredtext'
 __version__ = '$Id: $'
 
-import cocos.audio.SDL as SDL
 from ctypes import *
 
 from . import dll
+import cocos.audio.SDL as SDL
 
 SDL_GetTicks = dll.function(
     'SDL_GetTicks',
     '''Get the number of milliseconds since the SDL library initialization.
-
     Note that this value wraps if the program runs for more than ~49 days.
-
     :rtype: int
     ''',
     args=[],
@@ -26,11 +24,9 @@ SDL_GetTicks = dll.function(
 SDL_Delay = dll.function(
     'SDL_Delay',
     '''Wait a specified number of milliseconds before returning.
-
     :Parameters:
         `ms` : int
             delay in milliseconds
-
     ''',
     args=['ms'],
     arg_types=[c_uint],
@@ -48,35 +44,27 @@ _timercallback_ref = None   # Keep global to avoid possible GC
 def SDL_SetTimer(interval, callback):
     """Set a callback to run after the specified number of milliseconds has
     elapsed.
-
     The callback function is passed the current timer interval
     and returns the next timer interval.  If the returned value is the
     same as the one passed in, the periodic alarm continues, otherwise a
     new alarm is scheduled.  If the callback returns 0, the periodic alarm
     is cancelled.
-
     To cancel a currently running timer, call ``SDL_SetTimer(0, None)``.
-
     The timer callback function may run in a different thread than your
     main code, and so shouldn't call any functions from within itself.
-
     The maximum resolution of this timer is 10 ms, which means that if
     you request a 16 ms timer, your callback will run approximately 20 ms
     later on an unloaded system.  If you wanted to set a flag signaling
     a frame update at 30 frames per second (every 33 ms), you might set a
     timer for 30 ms::
-
           SDL_SetTimer((33/10)*10, flag_update)
-
     If you use this function, you need to pass `SDL_INIT_TIMER` to
     `SDL_Init`.
-
     Under UNIX, you should not use raise or use SIGALRM and this function
     in the same program, as it is implemented using ``setitimer``.  You
     also should not use this function in multi-threaded applications as
     signals to multi-threaded apps have undefined behavior in some
     implementations.
-
     :Parameters:
         `interval` : int
             Interval before callback, in milliseconds.
@@ -115,7 +103,6 @@ _timer_refs = {}        # Keep global to manage GC
 
 def SDL_AddTimer(interval, callback, param):
     """Add a new timer to the pool of timers already running.
-
     :Parameters:
         `interval` : int
             The interval before calling the callback, in milliseconds.
@@ -126,14 +113,11 @@ def SDL_AddTimer(interval, callback, param):
             passed in, the periodic alarm continues, otherwise a new alarm
             is scheduled.  If the callback returns 0, the periodic alarm is
             cancelled.  An example callback function is::
-
                 def timer_callback(interval, param):
                     print('timer called after %d ms.' % interval)
                     return 1000     # call again in 1 second
-
         `param` : any
             A value passed to the callback function.
-
     :rtype: int
     :return: the timer ID
     """
@@ -157,11 +141,9 @@ _SDL_RemoveTimer = dll.private_function(
 
 def SDL_RemoveTimer(t):
     """Remove one of the multiple timers knowing its ID.
-
     :Parameters:
         `t` : int
             The timer ID, as returned by `SDL_AddTimer`.
-
     """
     global _timer_refs
     _SDL_RemoveTimer(t)
