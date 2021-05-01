764acd018b8e329818ab9d24fe32fcd109676f58
harjay12 <57065385+harjay12@users.noreply.github.com>
2021-05-01 15:45:10 -0400

    - just new file

--- utest/test_fixed_issue.py
+++ utest/test_fixed_issue.py
@@ -1,11 +1,9 @@
 import pygame
 import sys
 
-from pygame.sprite import Sprite
-
 sys.path.insert(0, 'pyglet_mockup1')
 import pyglet
-__all__ = ['Sprite', ]  # Sprite class
+
 assert pyglet.mock_level == 1
 
 from cocos.director import director
