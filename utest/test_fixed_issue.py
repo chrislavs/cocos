40e0e941b904212d8aef1803e809507b30542f18
harjay12 <57065385+harjay12@users.noreply.github.com>
2021-05-03 12:36:48 -0400

    - All test for group issues done

--- utest/test_fixed_issue.py
+++ utest/test_fixed_issue.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import os
 from os import getenv
+
 sys.path.insert(0, 'pyglet_mockup1')
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import pyglet
@@ -9,6 +10,7 @@ import pyglet
 assert pyglet.mock_level == 1
 
 from cocos.director import director
+from cocos.audio.SDL import dll
 
 director.init()
 
@@ -16,16 +18,13 @@ director.init()
 class Test_Director(object):
 
     def test_scaled_resize_window(self):
-        window = 0
+        # window = 0
         assert director.window.width == 640
         assert director.window.height == 480
         if sys.platform == 'darwin':
-            viewportw, viewporth = director.window.get_viewport_size()
-            if viewportw > director.window.width:
-                vw, vh = viewportw, viewporth
-                window = vw, vh
+            window = director.window.get_viewport_size()
         else:
-            window = director.window.width, director.window.height
+            window = director.get_window_size()
         print(window)
 
         assert director.scaled_resize_window(director.window.width, director.window.width) == window
@@ -34,7 +33,3 @@ class Test_Director(object):
         default_size = director.window.width, director.window.height
         assert director.get_window_size() == default_size
 
-
-def test_pygame_version():
-    v = pygame.__version__
-    assert v == '1.9.6'
